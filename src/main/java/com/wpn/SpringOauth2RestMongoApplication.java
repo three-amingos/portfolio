3e7d6555eff14eb03a5e92f101bba4e2b8f4caa1
vdonkor <vincent.donkor@gmail.com>
2021-07-16 00:42:05 -0400

    added tomcat capabilities

--- src/main/java/com/wpn/SpringOauth2RestMongoApplication.java
+++ src/main/java/com/wpn/SpringOauth2RestMongoApplication.java
@@ -2,9 +2,11 @@ package com.wpn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//ensure that we can this app from tomcat
 @SpringBootApplication
-public class SpringOauth2RestMongoApplication {
+public class SpringOauth2RestMongoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOauth2RestMongoApplication.class, args);
