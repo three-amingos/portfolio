49dd9d8eb3ed3a9000083b3637de2ccdf932393d
tresamingo <84825312+tresamingo@users.noreply.github.com>
2021-07-22 14:29:51 -0400

    Update SpringOauth2RestMongoApplication.java

--- src/main/java/com/wpn/SpringOauth2RestMongoApplication.java
+++ src/main/java/com/wpn/SpringOauth2RestMongoApplication.java
@@ -2,9 +2,10 @@ package com.wpn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringOauth2RestMongoApplication {
+public class SpringOauth2RestMongoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOauth2RestMongoApplication.class, args);
